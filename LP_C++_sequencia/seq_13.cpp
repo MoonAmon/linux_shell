247c79b8f9843998c79dce015080b21b2e184a5f
MoonThi <misterofclans.mo@gmail.com>
2023-04-26 22:23:02 -0300

    26/04

--- LP_C++_sequencia/seq_13.cpp
+++ LP_C++_sequencia/seq_13.cpp
@@ -12,19 +12,29 @@ int main(int argc, char const *argv[])
     printf("Digite o denominador: ");
     scanf("%f",&den);
 
-    while (num>100||den>100)
+    while (num>1000||den>1000)
     {
-        printf("Digite um numero de 0 a 99!");
+        printf("Numero invalido, digite um numero de 0 a 999!");
+        return 0;
     }
     
 
-    if (num<100)
+    if (num<100 && den<100)
     {
         float X=num*100;
         float Y=X+den;
         double result=Y/100;
         printf("Resultado: %lf",result);
     }
+
+    if (num<1000 && den<1000)
+    {
+        float X=num*1000;
+        float Y=X+den;
+        double result=Y/1000;
+        printf("Resultado: %lf",result);
+    }
+    
 
     
 
