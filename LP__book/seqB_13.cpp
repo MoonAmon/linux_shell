de9e89424864bcedf6183acd46802784e5e380e7
MoonThi <misterofclans.mo@gmail.com>
2023-04-29 01:08:09 -0300

    (29/04)

---
+++ LP__book/seqB_13.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    float dist,temp,vel;
+
+    printf("digite a dist percorrida em Km: ");
+    scanf("%f",&dist);
+
+    printf("digite o espaçode tempo em min: ");
+    scanf("%f",&temp);
+
+    vel=(dist*1000)/(temp*60);
+
+    printf("a velocidade do projetil: %.2f m/s\n",vel);
+
+    return 0;
+}
