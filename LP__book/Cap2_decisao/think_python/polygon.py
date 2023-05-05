f46c895107d6a5bcccaf80b5ceeafe74df74e9c6
MoonThi <misterofclans.mo@gmail.com>
2023-05-05 13:53:21 -0300

    05/05

---
+++ LP__book/Cap2_decisao/think_python/polygon.py
@@ -0,0 +1,14 @@
+
+import turtle
+import math
+bob = turtle.Turtle()
+
+def polygon(t,n,length):
+    for i in range(8):
+        angle=360       
+        t.lt(70)
+        t.fd(180)
+     
+polygon(bob,7,70)
+
+turtle.mainloop()
