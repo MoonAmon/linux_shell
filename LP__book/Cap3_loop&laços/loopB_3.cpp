fcd5e6a3aaef5c48a034d0e77a4df54c86a90fda
MoonThi <misterofclans.mo@gmail.com>
2023-05-06 23:28:14 -0300

    06/05 loopB_3

--- LP__book/Cap3_loop&laços/loopB_3.cpp
+++ LP__book/Cap3_loop&laços/loopB_3.cpp
@@ -3,6 +3,20 @@
 
 int main(int argc, char const *argv[])
 {
+    int n,soma,I;
+
+    n=100;
+    soma=0;
+    I=1;
     
+    do
+    {
+        soma+=I;
+        I++;
+        printf("%d\n",soma);
+        
+    } while (I<=n);
+     
+
     return 0;
 }
