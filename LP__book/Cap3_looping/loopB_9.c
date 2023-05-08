60762564fde32a51b6760af68ba4c300be8671be
MoonSecond <132599723+MoonSecond@users.noreply.github.com>
2023-05-08 19:00:17 +0000

    08/05

--- LP__book/Cap3_looping/loopB_9.c
+++ LP__book/Cap3_looping/loopB_9.c
@@ -3,7 +3,20 @@
 
 int main(int argc, char const *argv[])
 {
-    int Cgran,total,
+    double Cgran = 0;
+    int casa = 1;
+    double total = 0;
+
+    do
+    {
+        Cgran=Cgran*2;
+        printf("Casa num: %d\n Total: %2.f\n Grões na casa atual: %2.f\n",casa,total,Cgran);
+        total=Cgran+total;
+        Cgran++;
+        casa++;
+    
+    } while (casa<=60);
+    
 
     return 0;
 }
