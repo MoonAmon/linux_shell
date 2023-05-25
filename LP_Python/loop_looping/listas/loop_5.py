3409487a45ea659a5000a1f8c0dbd2561038fe7f
MoonAmon <misterofclans.mo@gmail.com>
2023-05-25 12:56:54 -0300

    25/05 c++ loops lista 5,6,7

--- LP_Python/loop_looping/listas/loop_5.py
+++ LP_Python/loop_looping/listas/loop_5.py
@@ -1,11 +1,11 @@
-CountTotal = int(0)
-CountFem = int(0) 
-CountMan = int(0)
-CountKid = int(0)
-CountFemL = int(0)
-nLivros = int(0)
-CountIdade = int(0)
-CountMenNL = int(0)
+CountTotal = 0
+CountFem = 0 
+CountMan = 0
+CountKid = 0
+CountFemL = 0
+nLivros = 0
+CountIdade = 0
+CountMenNL = 0
 
 while True:
     sexo = str(input("Digite o sexo do participante: "))
@@ -13,9 +13,9 @@ while True:
     livros = int(input("Digite a quantidade de livros lidos em 2006: "))
     if idade<0:
         break
+    
     CountTotal+=1
     
-
     if sexo=="f":
         CountFem+= 1
 
@@ -36,5 +36,6 @@ while True:
         CountMenNL+=1
 
 MDmenl = CountIdade/CountMenNL
-MDnl = 
-print()
+PCnl = (nLivros/CountTotal) * 100
+
+print("qnt de livros lidos por crianças menores de 10 anos: %d\nnúmero de mulheres que leram 5 livros ou mais: %d\nmedia da idade dos homens que leram menos que 5 livros: %.2f\npercentual de pessoas que nao leram nenhum livro %.2f%%\n "%(CountKid,CountFemL,MDmenl,PCnl))
