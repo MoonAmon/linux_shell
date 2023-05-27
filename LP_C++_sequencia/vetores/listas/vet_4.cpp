25a9cd3a975d2582b85ae64bfbbf32eeb5993595
MoonAmon <misterofclans.mo@gmail.com>
2023-05-26 23:52:16 -0300

    26/05 lista vetores finalizado c++

--- LP_C++_sequencia/vetores/listas/vet_4.cpp
+++ LP_C++_sequencia/vetores/listas/vet_4.cpp
@@ -6,24 +6,23 @@ int main(int argc, char const *argv[])
     int n = 0;
     printf("\nDigite o número de alunos: ");
     scanf("%d",&n);
-    int G1[n],G2[n],soma[n],media[n];
+    float G1[n],G2[n],soma[n],media[n];
 
     for (int i = 0; i < n; i++)
     {
         printf("\nDigite a nota G1 do aluno %d: ",i+1);
-        scanf("%d",&G1[i]);
+        scanf("%f",&G1[i]);
         printf("\nDigite a nota G2 do aluno %d: ",i+1);
-        scanf("%d",&G2[i]);
+        scanf("%f",&G2[i]);
     }
     for (int i = 0; i < n; i++)
     {   
-        printf("%d\n",G1[i],G2[i]);
         soma[i]=G1[i]+G2[i];
         media[i]=soma[i]/2;
     }
     for (int i = 0; i < n; i++)
     {
-        printf("Aluno número %d\nMédia: %d\n Nota G1: %d\n Nota G2: %d\n",i+1,media[i],G1[i],G2[i]);
+        printf("Aluno número %d\nMédia: %.2f\n Nota G1: %.2f\n Nota G2: %.2f\n",i+1,media[i],G1[i],G2[i]);
     }
     
     
