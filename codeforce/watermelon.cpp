1b49c57bdd5eeb747d50ce6039e8e15d6a40630e
Thiago Amâncio <127698239+MoonAmon@users.noreply.github.com>
2023-05-28 01:35:52 +0000

    27/05 codeforce watermelon

---
+++ codeforce/watermelon.cpp
@@ -0,0 +1,20 @@
+        #include <stdio.h>
+        #include <stdlib.h>
+         
+        main (void){
+            
+            int resto,w;
+            
+            printf("\n");
+            scanf("%d",&w);
+            
+            resto = w/2;
+            
+            if (resto==0){
+                printf("YES\n");
+            } else {
+                printf("NO\n");
+            }
+            
+            return 0;
+        }
