b420aa3b0867ed563ff4da37b6238ce6fdfedbaf
MoonAmon <misterofclans.mo@gmail.com>
2023-06-13 00:26:25 -0300

    13/06 codeforces soma A

---
+++ codeforce/soma.py
@@ -0,0 +1,16 @@
+entrada = input().split()
+N = int(entrada[0])
+K = int(entrada[1])
+sequencia = list(map(int, input().split()))
+soma=0
+somatotal=0
+cont=0
+
+for num in sequencia:
+    somatotal+=num
+    if somatotal==K:
+        cont+=1
+    elif somatotal>K:
+        somatotal=0
+    print("somatotal: %d\nsoma: %d"%(somatotal,soma))
+print(cont)
