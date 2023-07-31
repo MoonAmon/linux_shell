5a7cafcfebbd3ac7dd2a3593fe8731488fd6426c
MoonAmon <misterofclans.mo@gmail.com>
2023-07-30 21:31:59 -0300

    revisão aplicação pratica matriz uni

--- LP_book/Cap5_apliMatriz/matriz_e.cpp
+++ LP_book/Cap5_apliMatriz/matriz_e.cpp
@@ -1,27 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-#include <ctime>
-
-int main(int argc, char const *argv[])
-{
-    int matrizA[20], matrizB[30];
-
-    for (int i = 0; i < 20; i++)
-    {
-     
-        matrizA[i] += rand() % 50;
-    }
-    for (int i = 0; i < 19; i++)
-    {
-        for (int j = i + 1; j < 20; j++)
-        {
-            if (matrizA[i]>matrizA[j])
-            {
-                int X = matrizA[i];
-                matrizA[i] = matrizA[j];
-                matrizA[j] = X;
-=======
 
 int main(int argc, char const *argv[])
 {
@@ -45,20 +23,11 @@ int main(int argc, char const *argv[])
                 x = matrizC[i];
                 matrizC[i] = matrizC[j];
                 matrizC[j] = x;
->>>>>>> 2f1782bc523f77da3b422bccf078b8919f0ac5a6
             }
             
         }
         
     }
-<<<<<<< HEAD
-     for (int i = 0; i < 20; i++)
-    {
-        printf("%d\n",matrizA[i]);
-    }
-    
-    
-=======
     for ( i = 0; i < 50; i++)
     {
         printf("%d",matrizC[i]);
@@ -67,6 +36,5 @@ int main(int argc, char const *argv[])
     
     
     
->>>>>>> 2f1782bc523f77da3b422bccf078b8919f0ac5a6
     return 0;
 }
