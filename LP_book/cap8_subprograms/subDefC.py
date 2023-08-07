b2473955e365b2c2e74fd6079027da2969459ea5
MoonAmon <misterofclans.mo@gmail.com>
2023-08-06 21:40:33 -0300

    tkinter new windows and frames

---
+++ LP_book/cap8_subprograms/subDefC.py
@@ -0,0 +1,39 @@
+import numpy as np
+from tkinter import *
+
+root = Tk()
+root.title("Diario")
+
+
+class DIARIO:
+    def __init__(self):
+        self.nome = None
+        self.nota = list(range(4))
+
+
+diario = [DIARIO() for _ in range(5)]
+
+
+def cadastro():
+    top_cad = Toplevel()
+    top_cad.title('Cadastro de alunos')
+    frame1 =
+
+
+
+def pesquisa(nomePesquisa):
+    achou = False
+    cont = 0
+    while achou is False and cont < 5:
+        pass
+
+
+def exibir():
+    pass
+
+
+button_cadastro = Button(root, text="Cadastro de Aluno", padx=30, pady=30, command=cadastro).pack()
+button_pesquisa = Button(root, text="Procurar Registros", padx=30, pady=30, command=pesquisa).pack()
+button_exibir = Button(root, text="Exibir Registros", padx=30, pady=30, command=exibir).pack()
+button_sair = Button(root, text="Sair", padx=30, pady=30)
+root.mainloop()
