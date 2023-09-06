089a61d8c75b297548698bf65941168a6e5267f5
MoonAmon <misterofclans.mo@gmail.com>
2023-09-06 15:36:03 -0300

    att project bash weather

---
+++ shell-linux/rx_poc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+today=$(date +%Y%m%d)
+weather_report=raw_data_$today
+city=london
+
+curl wttr.in/$city --output $weather_report
+grep °C $weather_report > tempetures.txt
+
+obs_tmp=$(head -n 1 tempetures.txt | cut -d "^" -f 1 | cut -c 43,44)
+echo "Temperatura atual: $obs_tmp °C"
+
+tm_tmp=
